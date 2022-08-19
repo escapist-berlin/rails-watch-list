ed2496a5bfa18be98adbfd7bc511f11e4fdc4e23
dk <denis.kolokolov@icloud.com>
2022-08-19 16:25:21 +0200

    watch list finish

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,21 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    # @movie = Movie.find(params[:movie_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
+  # get '/lists', to: 'lists#index'
+
+  # get '/lists/new', to: 'lists#new'
+  # post '/lists', to: 'lists#create'
+
+  # # get 'lists/:id/bookmarks/new', to
+
+  # get '/lists/:id', to: 'lists#show', as: 'list'
 end
